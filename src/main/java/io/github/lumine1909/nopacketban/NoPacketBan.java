a1aac4171108f8c2f01face709d442fbe497366c
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-02-27 17:09:34 -0800

    Start v2

--- src/main/java/io/github/lumine1909/nopacketban/NoPacketBan.java
+++ src/main/java/io/github/lumine1909/nopacketban/NoPacketBan.java
@@ -1,37 +1,34 @@
 package io.github.lumine1909.nopacketban;
 
 import io.github.lumine1909.nopacketban.metrics.Metrics;
+import io.papermc.paper.network.ChannelInitializeListenerHolder;
+import net.kyori.adventure.key.Key;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoPacketBan extends JavaPlugin {
 
+    private static final Key LISTENER_KEY = Key.key("nopacketban:listener");
+
     public static int RESERVED_PACKET_SIZE;
-    public static int MAX_NORMAL_PACKET_SIZE = 2097151;
-    public static int MAX_PACKET_SIZE = 8388608;
-    public static boolean hasVia = true;
+    public static int MAX_PACKET_SIZE = 2097151;
 
     private Metrics metrics;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+        ChannelInitializeListenerHolder.addListener(LISTENER_KEY, Injector::inject);
         metrics = new Metrics(this, 27531);
-        // Sorry for misusing words :(
         RESERVED_PACKET_SIZE = getConfig().getInt("reserved-packet-size", getConfig().getInt("preserved-packet-size", 262144));
         if (Bukkit.getPluginManager().getPlugin("ViaVersion") == null) {
             RESERVED_PACKET_SIZE = 0;
-            hasVia = false;
-        } else {
-            hasVia = true;
         }
-        MAX_NORMAL_PACKET_SIZE = 2097151 - RESERVED_PACKET_SIZE;
-        MAX_PACKET_SIZE = 8388608 - RESERVED_PACKET_SIZE;
+        MAX_PACKET_SIZE = 2097151 - RESERVED_PACKET_SIZE;
 
         for (Player player : Bukkit.getOnlinePlayers()) {
-            PlayerListener.injectPlayer(player);
+            Injector.injectPlayer(player);
         }
     }
 
@@ -40,9 +37,9 @@ public class NoPacketBan extends JavaPlugin {
         if (metrics != null) {
             metrics.shutdown();
         }
+        ChannelInitializeListenerHolder.removeListener(LISTENER_KEY);
         for (Player player : Bukkit.getOnlinePlayers()) {
-            PlayerListener.uninjectPlayer(player);
+            Injector.uninjectPlayer(player);
         }
-        PlayerListener.injectedPlayer.clear();
     }
 }
---
+++ src/main/java/io/github/lumine1909/nopacketban/handler/ServerHandler.java
@@ -0,0 +1,37 @@
+package io.github.lumine1909.nopacketban.handler;
+
+import io.netty.channel.*;
+import net.kyori.adventure.text.JoinConfiguration;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minecraft.network.PacketEncoder;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+
+import static net.kyori.adventure.text.Component.join;
+import static net.kyori.adventure.text.Component.text;
+
+public class ServerHandler extends ChannelDuplexHandler {
+
+    private final Channel channel;
+
+    public ServerHandler(Channel channel) {
+        this.channel = channel;
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable exception) throws Exception {
+        if (exception instanceof io.netty.handler.codec.EncoderException && exception.getCause() instanceof PacketEncoder.PacketTooLargeException packetTooLargeException) {
+            final Packet<?> packet = packetTooLargeException.getPacket();
+            if (packet.hasLargePacketFallback()) {
+                super.exceptionCaught(ctx, exception);
+                return;
+            }
+        }
+        channel.writeAndFlush(new ClientboundSystemChatPacket(join(JoinConfiguration.newlines(),
+            text("An error occurred on sending packet.", NamedTextColor.RED),
+            text("Please report to server admin if you believe this is in error.", NamedTextColor.RED),
+            text("Or consider you are being packet kicked.", NamedTextColor.RED),
+            text("Details: " + exception.getMessage(), NamedTextColor.RED)
+        ), false));
+    }
+}
