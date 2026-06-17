4744fb6c6fd042c0feecfc48ea76730588018105
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-06-16 17:33:42 -0700

    Fix typo

--- src/main/java/io/github/lumine1909/nopacketban/handler/ClientHandler.java
+++ src/main/java/io/github/lumine1909/nopacketban/handler/ClientHandler.java
@@ -37,7 +37,7 @@ public class ClientHandler extends ChannelDuplexHandler {
         private static final String NOTICE = """
             PacketTooLarge - %s is out of the max %s.
             This is not the vanilla limit, but because ViaVersion is installed on the server, the server sets %s as a reserve size to prevent client-side DecoderExceptions caused by unexpected size inflation.
-            If you believe the preserved size is too large, please report to the server admin.\s""";
+            If you believe the reserved size is too large, please report to the server admin.\s""";
 
         StacklessOversizeException(int packetLength) {
             super(NOTICE.formatted(packetLength, MAX_PACKET_SIZE, RESERVED_PACKET_SIZE));
